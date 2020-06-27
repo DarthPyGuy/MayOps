e50595e2e16fcf9348f1e275a0d6522c366f9e69
John May <JMay526@gmail.com>
2020-06-27 01:26:44 -0700

    fixed path to MayOps homepage

--- May_Ops/urls.py
+++ May_Ops/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include("homepage.urls")),
+    path('', include("homepage.urls")),
 ]
