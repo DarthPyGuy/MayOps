d51f11e08962b7f68048e72a1a31a8f9f3968426
John May <JMay526@gmail.com>
2020-06-26 19:43:33 -0700

    fixed HttpResponse and path

--- homepage/views.py
+++ homepage/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import httpResponse
+from django.http import HttpResponse
 
 def home(request):
-    return httpResponse("<h1>MayOps</h1>")
+    return HttpResponse("<h1>MayOps</h1>")
