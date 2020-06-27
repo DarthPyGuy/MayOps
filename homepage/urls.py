deee70482828ff73e8774482dc54945b423b309e
John May <JMay526@gmail.com>
2020-06-26 23:56:55 -0700

    created a blog page off the homepage

--- homepage/urls.py
+++ homepage/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('', views.home, name="homepage"),
+    path('blog/', views.blog, name="blog"),
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("<h1>MayOps</h1>")
+
+def blog(request):
+    return HttpResponse("<h1>Blog</h1>")
