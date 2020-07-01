dfe4b9c6f7bfc37cff7cdbc9571f0ac71997f867
John May <JMay526@gmail.com>
2020-07-01 14:25:59 -0700

    Updated blog templates. Created 2 dummy posts, list of classes and added 'context' dict to home.views

--- homepage/views.py
+++ homepage/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'John May',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'June 29, 2020'
+    },
+     {
+        'author': "John May",
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'Tbd 00, 2020'
+    }
+]
+
 def home(request):
     return render(request, 'homepage/home.html')
 
 def blog(request):
-    return render(request, 'homepage/blog.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'homepage/blog.html', context)
 
 def about(request):
     return render(request, 'homepage/about.html')
