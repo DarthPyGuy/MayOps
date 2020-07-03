a67d3a37102017aaee7544afade9017392f0aac0
John May <JMay526@gmail.com>
2020-07-02 23:32:15 -0700

    Created static directory, navigation bar and added global styles.

--- homepage/urls.py
+++ homepage/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="homepage"),
-    path('blog/', views.blog, name="blog"),
-    path('about/', views.about, name="about"),
+    path('', views.home, name="MayOps-home"),
+    path('blog/', views.blog, name="MayOps-blog"),
+    path('about/', views.about, name="MayOps-about"),
 ]
