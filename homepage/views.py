1d41b3b09eb22398a3fe1fcc9bc0748f6f3918dc
John May <JMay526@gmail.com>
2020-07-03 18:28:22 -0700

    Created Post model in models.py. Added user data to database. Added date filter on blog post date. Deleted dummy posts data dicts. Registered models to show on the admin page.

--- homepage/views.py
+++ homepage/views.py
@@ -1,29 +1,17 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author': 'John May',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'June 29, 2020'
-    },
-     {
-        'author': "John May",
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'Tbd 00, 2020'
-    }
-]
 
 def home(request):
     return render(request, 'homepage/home.html')
 
+
 def blog(request):
     context = {
-        'posts': posts,
-        'title': 'Blog'
+        'posts': Post.objects.all()
     }
     return render(request, 'homepage/blog.html', context)
+
 
 def about(request):
     return render(request, 'homepage/about.html', {'title': 'About Me'})
