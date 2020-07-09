e925a9d974388d9cd1618e76626252d15dffdb70
John May <JMay526@gmail.com>
2020-07-09 13:38:26 -0700

    Added pagination to blog. Created userpostlistview also with pagination.

--- homepage/urls.py
+++ homepage/urls.py
@@ -5,12 +5,14 @@ from .views import (
     PostDetailView,
     PostCreateView,
     PostUpdateView,
-    PostDeleteView
+    PostDeleteView,
+    UserPostListView
 )
 
 urlpatterns = [
     path('', views.home, name="MayOps-home"),
     path('blog/', PostListView.as_view(), name="MayOps-blog"),
+    path('user/<str:username>', UserPostListView.as_view(), name="user-posts"),
     path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
     path('post/new/', PostCreateView.as_view(), name="post-create"),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name="post-update"),
--- homepage/views.py
+++ homepage/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 from .models import Post
 from django.views.generic import (
     ListView,
@@ -27,6 +28,18 @@ class PostListView(ListView):
     template_name = 'homepage/blog.html'
     context_object_name = 'posts'
     ordering = ["-date_posted"]
+    paginate_by = 5
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'homepage/user_posts.html'
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by("-date_posted")
 
 
 class PostDetailView(DetailView):
