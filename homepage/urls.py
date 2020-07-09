2600c2412fd90cfccdf7c561833e93e15e00865d
John May <JMay526@gmail.com>
2020-07-09 01:04:56 -0700

    Added create, update, and delete posts with class based views.

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
 from . import views
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
+)
 
 urlpatterns = [
     path('', views.home, name="MayOps-home"),
-    path('blog/', views.blog, name="MayOps-blog"),
+    path('blog/', PostListView.as_view(), name="MayOps-blog"),
+    path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
+    path('post/new/', PostCreateView.as_view(), name="post-create"),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name="post-update"),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name="post-delete"),
     path('about/', views.about, name="MayOps-about"),
 ]
