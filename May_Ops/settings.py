b426b5e374d1098d3621d57736f4b5a60b8c3bb1
John May <JMay526@gmail.com>
2020-07-15 14:56:18 -0700

    settings updated .env

--- May_Ops/settings.py
+++ May_Ops/settings.py
@@ -154,3 +154,6 @@ AWS_DEFAULT_ACL = None
 DEFAULT_FILES_STORAGE = 'storages.backends.s3boto3.S3boto3Storage'
 
 django_heroku.settings(locals())
+
+from boto.s3.connection import S3Connection
+s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
