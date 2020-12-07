02c821c1135fbd251496a19a0e0c5cad63ac4736
John May <jmay526@gmail.com>
2020-12-07 11:39:22 -0800

    setup raise Http404 if object doesn’t exist, remove hardcode URLs in templates, Namespace url names

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, Http404
 
 from .models import Question
 
@@ -10,7 +10,8 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
@@ -18,4 +19,3 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
-
