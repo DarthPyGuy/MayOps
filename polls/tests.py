626d6692e34e54bb92ca1fbc62d8abf520a95b32
John May <jmay526@gmail.com>
2020-12-08 17:01:24 -0800

    more tests

--- polls/tests.py
+++ polls/tests.py
@@ -16,3 +16,21 @@ class QuestionModelTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() returns False for questions whose
+        pub_date is older than 1000 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=1000, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        """
+        test_was_published_recently returns True for questions whose
+        pub_date is within the last 1000 days.
+        """
+        time = timezone.now() - datetime.timedelta(days=999, hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
