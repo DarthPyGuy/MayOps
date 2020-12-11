cfb62d66396c6075b8acb9f38e044a758e3c6120
John May <jmay526@gmail.com>
2020-12-10 19:01:51 -0800

    1 -> 1000 for "recent"

--- May_Ops/settings.py
+++ May_Ops/settings.py
@@ -70,7 +70,7 @@ ROOT_URLCONF = 'May_Ops.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
